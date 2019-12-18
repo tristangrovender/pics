31c820c4f72a05dc0ff2e680af5ce27a86e430ab
Tristan Grovender <therealtristangrovender@gmail.com>
2019-12-18 16:37:40 -0600

    reformatted to controlled element - storing value in state vs the DOM

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,11 +1,24 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+    state = { term: "" };
+
     render() {
         return (
-            <div>
-                <form>
-                    <input type="text" />
+            <div className="ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Image Search</label>
+                        <input
+                            type="text"
+                            value={this.state.term}
+                            onChange={e =>
+                                this.setState({
+                                    term: e.target.value.toUpperCase()
+                                })
+                            }
+                        />
+                    </div>
                 </form>
             </div>
         );
